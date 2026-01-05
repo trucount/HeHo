45feddae48e93913f394c53155fcdad35e940d86
appointease.dev <appointease.dev@gmail.com>
2026-01-05 07:51:48 +0000

    casde

--- lib/supabase/config.ts
+++ lib/supabase/config.ts
@@ -1,5 +1,5 @@
 
 export const supabaseOAuthConfig = {
-  clientId: "68bd9c63-2a70-4807-a8ef-030d705262bc",
-  clientSecret: "sba_196493420ac6aea47c378cb23508d416f1c5a902",
+  clientId: process.env.SUPABASE_OAUTH_CLIENT_ID!,
+  clientSecret: process.env.SUPABASE_OAUTH_CLIENT_SECRET!,
 };
